9696a956acaf4c4b1e2ffa3e7dddabaf01f195dc
VladiusVostokus <kovalenkov222@gmail.com>
2024-03-14 18:01:46 +0200

    Split input string, to get an array

--- implementation.go
+++ implementation.go
@@ -5,5 +5,7 @@ import "fmt"
 // TODO: document this function.
 // PrefixToPostfix converts
 func PrefixToPostfix(input string) (string, error) {
+	
+	arr := strings.Split(input)
 	return "TODO", fmt.Errorf("TODO")
 }
