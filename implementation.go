29344823a8304abb58e38c32757f79ca8429ad0b
VladiusVostokus <kovalenkov222@gmail.com>
2024-03-15 11:08:48 +0200

    Initial idea of function
    To calculate postfix expression, we neet an array to put numbers here.
    And if some value of input string isn't equal to number(assuming it is a
    arithmetic operation) we will do calculation. However, this func doesn't
    work properly yet, it's just to show the main idea.

--- implementation.go
+++ implementation.go
@@ -1,11 +1,39 @@
 package lab2
 
-import "fmt"
+import (
+	"fmt"
+	"unicode"
+	"strconv"
+)
 
 // TODO: document this function.
 // PrefixToPostfix converts
 func CalculatePostfix(input string) (int, error) {
 
 	arr := strings.Split(input)
-	return 0, fmt.Errorf("TODO")
+	nums := make([]int, 0,3)
+	var res int
+
+	for _, v := range arr {
+		if unicode.IsNumber(v) {
+			num := strconv.Atoi(v)
+			nums = append(nums, num)
+		}
+		n1 := nums[0]
+		n2 := nums[1]
+		switch v {
+		case "+":
+			res = n1 + n2
+		case "-":
+			res = n1 - n2
+		case "*":
+			res = n1 * n2
+		case "/":
+			res = n1 / n2
+		case "^":
+		default:
+			//error
+		}
+	}
+	return res, fmt.Errorf("TODO")
 }
