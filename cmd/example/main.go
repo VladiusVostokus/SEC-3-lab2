cd09c2b89de46b3ada5f50f195d039cb835e9910
VladiusVostokus <kovalenkov222@gmail.com>
2024-03-23 12:47:38 +0200

    Add flag -f and improved work of all flags

--- cmd/example/main.go
+++ cmd/example/main.go
@@ -7,13 +7,24 @@ import (
 	"os"
 	"strconv"
 )
-
+/*
 var (
 	inputExpression = flag.String("e", "", "Expression to compute")
+	inputFile = flag.String("f", "", "File with expression")
 	outputFile = flag.String("o", "", "File to output result")
+)*/
+
+var (
+	inputExpression string
+	inputFile string
+	outputFile string
 )
 
 func main() {
+	flag.StringVar(&inputExpression, "e", "", "Expression to compute")
+	flag.StringVar(&inputFile, "f", "","File with expression")
+	flag.StringVar(&outputFile, "o", "", "File to output result")
+
 	flag.Parse()
 
 	// TODO: Change this to accept input from the command line arguments as described in the task and
@@ -23,15 +34,10 @@ func main() {
 	//           Output: {construct io.Writer according the command line parameters},
 	//       }
 	//       err := handler.Compute()
-	expression := flag.Arg(0)
-	file := flag.Arg(2)
-
-	res, _ := lab2.CalculatePostfix(expression)
-	fmt.Println(res)
-	fmt.Println(file)
 	
-	if file != " " {
-		f, err := os.Create(file)
+	if outputFile != "" {
+		res, _ := lab2.CalculatePostfix(inputExpression)
+		f, err := os.Create(outputFile)
 		if err != nil {
 			panic(err)
 		}
@@ -39,5 +45,14 @@ func main() {
 		if err2 != nil {
 			panic(err)
 		}
+	} else {
+		f, err := os.Open(inputFile)
+		if err != nil {
+			panic(err)
+		}
+		barr := make([]byte, 5)
+		f.Read(barr)
+		res, _ := lab2.CalculatePostfix(string(barr))
+		fmt.Println(res)
 	}
 }
